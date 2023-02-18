e1acfa30c340e928956d58eb26ecd6c88bb6f97b
david_esparza_guerrero <azrapse@gmail.com>
2023-02-18 18:28:37 +0200

    Initial commit.

---
+++ TestRun/TestRun/BrowserSelector.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestRun
+{
+    public static class BrowserSelector
+    {
+        public readonly static Dictionary<string, (Func<IDriverConfig> Config, Func<IWebDriver> Driver)> SupportedBrowsers = new Dictionary<string, (Func<IDriverConfig> Config, Func<IWebDriver> Driver)>
+        {
+            { "Chrome", (()=>new ChromeConfig(), ()=>new ChromeDriver()) },
+            { "Firefox", (() => new FirefoxConfig(), ()=>new FirefoxDriver()) },
+            { "Edge", (()=> new EdgeConfig(), ()=>new EdgeDriver()) },
+            { "Internet Explorer", (()=>new InternetExplorerConfig(), ()=>new InternetExplorerDriver()) },
+        };
+
+        public static void AwaitForElement(this IWebDriver driver, string elementId)
+        {
+            var wait = new WebDriverWait(driver, timeout: TimeSpan.FromSeconds(30))
+            {
+                PollingInterval = TimeSpan.FromSeconds(5),
+            };
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            wait.Until(drv => drv.FindElement(By.Id(elementId)));
+        }
+    }
+
+}
---
+++ TestRun/TestRun/TestsCollection.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestRun
+{    
+    public class TestsCollection
+    {
+        public static void Start(Func<IDriverConfig> configGetter, Func<IWebDriver> driverGetter)
+        {
+            var config = configGetter();
+            var manager = new DriverManager().SetUpDriver(config);
+            
+            IWebDriver driver;
+            try 
+            { 
+                driver = driverGetter();
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Problems initializing browser driver.", ex);
+            }
+            // Making sure the driver is released at the end of execution.
+            //using (driver)
+            {
+                driver.Navigate().GoToUrl("https://azrapse.es/tor/sheet.html");
+                driver.AwaitForElement("nameInput");
+                
+            }
+        }
+
+        public static bool Test1(IWebDriver driver)
+        {
+            return false;
+        }
+    }
+}
